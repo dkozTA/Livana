c5c11e9871cb9056f277675f9d4f43eac1c94281
nguyendamkien <kientrungx10st@gmail.com>
2025-05-19 23:23:43 +0700

    success

--- app/src/main/java/com/example/myapplication/ui/activities/CreatePropertyActivity.java
+++ app/src/main/java/com/example/myapplication/ui/activities/CreatePropertyActivity.java
@@ -1,9 +1,14 @@
 package com.example.myapplication.ui.activities;
 
 import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -145,14 +150,23 @@ public class CreatePropertyActivity extends AppCompatActivity {
     }
 
     private void showSuccessDialog() {
-        new AlertDialog.Builder(this)
-                .setTitle("Thành công")
-                .setMessage("Phòng đã được tạo thành công!")
-                .setPositiveButton("OK", (dialog, which) -> {
-                    dialog.dismiss();
-                    finish(); // hoặc chuyển sang Activity khác
-                })
-                .show();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(this).inflate(R.layout.dialog_success, null);
+        builder.setView(view);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        Button btnOk = view.findViewById(R.id.btnOk);
+        btnOk.setOnClickListener(v -> {
+            dialog.dismiss();
+            Intent intent = new Intent(CreatePropertyActivity.this, HostMainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        });
     }
+
 
 }
